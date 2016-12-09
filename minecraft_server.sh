e46a9996cd509c79e5532977fdd302032bb61444
Zhen Zhi Lee <zzyzy@ubuntu-512mb-sgp1-01.localdomain>
2016-12-09 06:15:16 +0000

    Save and stopped server at Fri Dec  9 06:15:16 UTC 2016

--- minecraft_server.sh
+++ minecraft_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git pull
-java -Xmx512M -Xms512M -jar minecraft_server.jar nogui
+java -Xmx384M -Xms384M -jar minecraft_server.jar nogui
 git add .
 git commit -m "Save and stopped server at $(date)"
 git push
